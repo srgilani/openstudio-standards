c5e33ebd20910973f6c3c7c1231939a2f771eda9
JeanGuo92 <52754647+JeanGuo92@users.noreply.github.com>
2019-12-17 15:42:20 -0500

    reconstruct courthouse ruby code

--- lib/openstudio-standards.rb
+++ lib/openstudio-standards.rb
@@ -302,6 +302,7 @@ module OpenstudioStandards
   require_relative "#{proto}/common/buildings/Prototype.MediumOfficeDetailed"
   require_relative "#{proto}/common/buildings/Prototype.SmallOfficeDetailed"
   require_relative "#{proto}/common/buildings/Prototype.Laboratory"
+  require_relative "#{proto}/common/buildings/Prototype.Courthouse"
 
   # NECB Building Types
   require_relative "#{proto}/common/prototype_metaprogramming.rb"

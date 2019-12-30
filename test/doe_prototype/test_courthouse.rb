11db0f023aba939c199eed93d70f2a1682422970
JeanGuo92 <52754647+JeanGuo92@users.noreply.github.com>
2019-12-30 12:17:10 -0500

    Update test_courthouse.rb

--- test/doe_prototype/test_courthouse.rb
+++ test/doe_prototype/test_courthouse.rb
@@ -1,7 +1,7 @@
 require_relative '../helpers/minitest_helper'
 require_relative '../helpers/create_doe_prototype_helper'
 
-class Courthouse < CreateDOEPrototypeBuildingTest
+class TestCourthouse < CreateDOEPrototypeBuildingTest
   building_types = ['Courthouse']
   templates = ['DOE Ref Pre-1980','DOE Ref 1980-2004','90.1-2007','90.1-2013']
   climate_zones = ['ASHRAE 169-2013-2A','ASHRAE 169-2013-3B','ASHRAE 169-2013-5A','ASHRAE 169-2013-8A']
@@ -11,4 +11,5 @@ class Courthouse < CreateDOEPrototypeBuildingTest
   compare_results = false
   debug = false
   Courthouse.create_run_model_tests(building_types, templates, climate_zones, epw_files, create_models, run_models, compare_results, debug)
+
 end

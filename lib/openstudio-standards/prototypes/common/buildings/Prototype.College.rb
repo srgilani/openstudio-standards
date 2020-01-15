ab5faba4434b3c55f442ee667e323f3becd82df4
JeanGuo92 <52754647+JeanGuo92@users.noreply.github.com>
2020-01-15 18:09:55 -0500

    infiltration added

--- lib/openstudio-standards/prototypes/common/buildings/Prototype.College.rb
+++ lib/openstudio-standards/prototypes/common/buildings/Prototype.College.rb
@@ -4,13 +4,35 @@
 # building types.
 module College
   def model_custom_hvac_tweaks(building_type, climate_zone, prototype_input, model)
-    #   Infiltration		"Peak: 0.2016 cfm/sf of above grade exterior wall surface area, adjusted by wind (when fans turn off)
-    # Off Peak: 25% of peak infiltration rate (when fans turn on)
-    # Additional infiltration through building entrance"		
+
 
   end
 
   def model_custom_internal_load_tweaks(building_type, climate_zone, prototype_input, model)
+    #   SpaceInfiltration		"Peak: 0.2016 cfm/sf of above grade exterior wall surface area, adjusted by wind (when fans turn off)
+    # Off Peak: 25% of peak infiltration rate (when fans turn on)
+    # Additional infiltration through building entrance"
+    infil_schedule = model.getScheduleRulesetByName("College INFIL_SCH_PNNL")
+    model.getSpaceTypes.sort.each do |space_type|
+      infil_flow_rates = space_type.getSpaceInfiltrationDesignFlowRates
+      unless infil_flow_rates.empty?
+        infil_flow_rates.each do |infil_flow_rate|
+          infil_flow_rate.setSpaceInfiltrationDesignFlowRate(0.2016)
+          infil_flow_rate.setSchedule(infil_schedule)
+        end
+      else # create a new infiltration object for the spacetypes without any infiltration defined
+        infil_flow_rate = Openstudio::Model::getSpaceInfiltrationDesignFlowRate.new(model)
+        infil_flow_rate.setSpaceInfiltrationDesignFlowRate(0.2016)
+        infil_flow_rate.setSchedule(infil_schedule)
+        infil.infil_flow_rate.setSpaceType(space_type)
+      end
+    end
+
+    building_entrance_lobby = model.getSpaceByName("CB_ Entrance Lobby_F0")
+    lobby_door_leakage_area = Openstudio::Model::getSpaceInfiltrationEffectiveLeakageArea.new(model)
+    lobby_door_leakage_area.setEffectiveAirLeakageArea(5) # Need modification
+    lobby_door_leakage_area.setSpace(building_entrance_lobby)
+
     #Plugload:Average power density (W/ft2)		See under Zone Summary (MISSING)
     #Schedule		See under Schedules
     #Zone Control Type: minimum supply air at 30% of the zone design peak supply air
